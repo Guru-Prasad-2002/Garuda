773e556b87bf6ea34e108b049b062a29ebd70603
Guru <gurujissrr@gmail.com>
2023-07-07 23:08:12 +0530

    Open Day

--- delete.py
+++
@@ -1,7 +0,0 @@
-import pickle
-with open("known_faces.pickle", "rb") as file:
-        known_data = pickle.load(file)
-known_encodings = known_data["encodings"]
-known_names = known_data["names"]
-
-print(known_data)

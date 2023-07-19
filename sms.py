4776d715433f298a360fff843e26591bc5be4673
Sai Harshit.B <89305971+Rostemelov@users.noreply.github.com>
2023-07-19 21:50:16 +0530

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,20 +2,10 @@ from boltiot import Sms, Bolt
 import sys 
 #
 val=sys.argv[1]
-mybolt = Bolt('e6a9fef6-cb95-41e4-acfe-3953e1bb9467', 'BOLT848710')
-sms = Sms('ACbfaef5952757e880587ca693232ac58a', 'db881f9cf65cc1465b43e521563a508f', '+91 8431003590', '+18142125826')
+mybolt = Bolt('BOLT API KEY', 'BOLT ID')
+sms = Sms('Twilio Account Number', 'API KEY', 'Target Phone number', 'Twilio phone number')
 response = mybolt.digitalWrite('0', 'HIGH')
 response = sms.send_sms(val)
 
 def status():
     response=mybolt.digitalRead('2')
-
-
-
-# from boltiot import Sms, Bolt
-
-# mybolt = Bolt('e6a9fef6-cb95-41e4-acfe-3953e1bb9467', 'BOLT848710')
-# sms = Sms('ACbfaef5952757e880587ca693232ac58a', 'db881f9cf65cc1465b43e521563a508f', '+91 8431003590', '+18142125826')
-# response = mybolt.digitalWrite('0', 'HIGH')
-# response = mybolt.digitalWrite('0', 'LOW')
-# response = sms.send_sms("L")
